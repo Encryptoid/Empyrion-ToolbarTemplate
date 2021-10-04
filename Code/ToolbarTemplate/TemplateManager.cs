6a68f0e35436205d4f99a63639be445b06a12891
Encryptoid <penshhan@gmail.com>
2021-10-04 20:08:17 +0100

    Initial code commit

---
+++ Code/ToolbarTemplate/TemplateManager.cs
@@ -0,0 +1,66 @@
+﻿using Eleon.Modding;
+using EmpyrionModdingFramework;
+using InventoryManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace InventoryTemplate
+{
+    public class TemplateManager
+    {
+        private InventoryManager _invManager;
+        private Action<string> _log;
+
+        public TemplateManager(InventoryManager invManager, Action<string> logFunc)
+        {
+            _invManager = invManager;
+            _log = logFunc;
+        }
+
+        internal void SaveInventoryTemplate(string steamId, PlayerInfo playerInfo)
+        { 
+            _invManager.SaveBar(steamId, playerInfo.toolbar);
+        }
+
+        internal Inventory LoadTemplateInventory(PlayerInfo playerInfo)
+        {
+            //Combine allItems from bag and bar
+            var allItems = playerInfo.bag.ToInvetoryItems();
+            allItems.AddRange(playerInfo.toolbar.ToInvetoryItems());
+
+            //Load template
+            var barTemplate = _invManager.LoadBar(playerInfo.steamId).ToInvetoryItems();
+            var newToolbar = new List<ItemStack>();
+
+            //Match items from template to allItems
+            foreach (var templateItem in barTemplate)
+            {
+                var match = allItems.FirstOrDefault(curItem => curItem.Id == templateItem.Id);
+                if (match == null) continue;
+
+                match.SlotId = templateItem.SlotId;
+                //Add to toolbar, clear from original place
+                newToolbar.Add(match.ToItemStack());
+                match.ClearItem();
+            }
+            var clearedItems = allItems.RemoveAll(m => m.Count == 0); //Remove cleared items
+            _log("Removed " + clearedItems);
+
+            //Converts inventory overflow to toolbar
+            while (allItems.Count() > 40)
+            {
+                _log("Item overflow occurred");
+                var item = allItems.First();
+                newToolbar.Add(item.ToItemStack());
+                allItems.RemoveAt(0);
+            }
+
+            //Create new inv
+            var newBag = allItems.CleanSlotIds(true).Select(item => item.ToItemStack()).ToArray();
+            var newInv = new Inventory() { playerId = playerInfo.entityId, toolbelt = newToolbar.ToArray().CleanSlotIds(false), bag = newBag };
+
+            return newInv;
+        }
+    }
+}
---
+++ Code/ToolbarTemplate/ToolbarTemplate.cs
@@ -0,0 +1,68 @@
+﻿using Eleon;
+using Eleon.Modding;
+using EmpyrionModdingFramework;
+using InventoryManagement;
+using ModLocator;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace InventoryTemplate
+{
+    public class ToolbarTemplate : EmpyrionModdingFrameworkBase
+    {
+        private TemplateManager _templateManager;
+
+        protected override void Initialize()
+        {
+            Log("ToolbarTemplate is running from folder: " + Directory.GetCurrentDirectory());
+
+            ModName = "ToolbarTemplate";
+
+            var modLocator = new FolderLocator((string s) => Log(s));
+
+            var inventoryManager = new InventoryManager(new CsvManager(modLocator.GetDatabaseFolder(ModName), (string s) => Log(s)));
+            _templateManager = new TemplateManager(inventoryManager, (string s) => Log(s));
+
+            var chatPrefix = "!";
+            CommandManager.CommandPrexix = chatPrefix;
+
+            CommandManager.CommandList.Add(new ChatCommand($"t save", (I) => SaveTemplate(I)));
+            CommandManager.CommandList.Add(new ChatCommand($"t", (I) => LoadTemplate(I)));
+        }
+
+        private async Task SaveTemplate(MessageData data)
+        {
+            Log($"SaveTemplate called with entityId: {data.SenderEntityId}");
+            PlayerInfo player = (PlayerInfo)await RequestManager.SendGameRequest(CmdId.Request_Player_Info, new Id() { id = data.SenderEntityId });
+            Log("Retrieved player info.");
+
+            _templateManager.SaveInventoryTemplate(player.steamId, player);
+            Log("Saved template");
+
+            await MessagePlayer(data.SenderEntityId, $"Saved template.", 5, MessagerPriority.Yellow);
+        }
+
+        private async Task LoadTemplate(MessageData data)
+        {
+            Log($"LoadTemplate called with entityId: {data.SenderEntityId}");
+            PlayerInfo player = (PlayerInfo)await RequestManager.SendGameRequest(CmdId.Request_Player_Info, new Id() { id = data.SenderEntityId });
+            Log("Retrieved player info.");
+
+            Inventory template;
+            try 
+            {
+                template = _templateManager.LoadTemplateInventory(player); 
+            }
+            catch (Exception e)
+            {
+                Log($"Error loading template: {e}");
+                return;
+            }
+
+            await RequestManager.SendGameRequest(CmdId.Request_Player_SetInventory, template);
+            await MessagePlayer(data.SenderEntityId, $"Loaded template.", 5, MessagerPriority.Yellow);
+            Log("Loaded template");
+        }
+    }
+}
